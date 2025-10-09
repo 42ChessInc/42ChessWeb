55123ae824b256fa68106dd7d45b933b582c16e7
denionline <danielximenes164@gmail.com>
2025-10-09 18:52:37 +0100

    feat: Add the DefaultLayout as Default on router

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,13 +2,16 @@ import {Route, Routes} from "react-router-dom";
 import {Home} from "./pages/Home";
 import {Error404} from "./pages/Error404";
 import {UnderConstruction} from "./pages/UnderConstruction";
+import {DefaultLayout} from "./layouts/DefaultLayout";
 
 export const Router = () => {
 	return (
 		<Routes>
-			<Route path="/" element={<UnderConstruction />} />
-			<Route path="/home" element={<Home />} />
-			<Route path="*" element={<Error404 />} />
+			<Route path="/" element={<DefaultLayout />}>
+				<Route path="/" element={<UnderConstruction />} />
+				<Route path="/home" element={<Home />} />
+				<Route path="*" element={<Error404 />} />
+			</Route>
 		</Routes>
 	);
 };
