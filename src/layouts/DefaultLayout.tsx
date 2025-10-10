b75a1cfc60bd4817a3f58f194854a7375b773f25
JulioSouza09 <juliosouza.ux@gmail.com>
2025-10-10 21:37:04 +0100

    Created componenent and added to defaultLayout

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../components/Footer"
 
 export const DefaultLayout = () => {
   return (
     <main className="h-screen w-screen bg-white">
       {/* <NavBar /> */}
       <Outlet />
+      <Footer />
     </main>
   );
 };
