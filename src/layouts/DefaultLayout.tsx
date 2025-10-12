b5bf36a30e16751cbac722bc7fe9cdb749daee99
leoc01 <leonardocampetti@gmail.com>
2025-10-12 14:19:50 +0100

    fix: resolve conclicts

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import { Header } from '../components/Header';
-import { Queen } from '../components/Queen';
+import Queen from '../components/Queen';
 import { Footer } from '../components/Footer';
 
 export const DefaultLayout = () => {
