bcc29162ed8f04594bc3366cfd0d539ce4b425b4
leoc01 <leonardocampetti@gmail.com>
2025-10-12 14:04:41 +0100

    feat: add queen with texture

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,12 +1,12 @@
-import {Outlet} from "react-router-dom";
-import {Header} from "../components/Header";
-import {Queen3d} from "../components/Queen";
+import { Outlet } from 'react-router-dom';
+import { Header } from '../components/Header';
+import Queen from '../components/Queen';
 
 export const DefaultLayout = () => {
 	return (
 		<main className="relative h-screen w-screen bg-[var(--color-background)]">
 			<Header />
-			<Queen3d />
+			<Queen />
 			<Outlet />
 		</main>
 	);
