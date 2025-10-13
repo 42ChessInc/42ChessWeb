a784abc89e1b3445e05e6907eca75467bce179d6
daniel <danieldafonseca@sapo.pt>
2025-10-13 09:47:33 +0100

    added responsiveness

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,49 +1,31 @@
-import {HorseIcon, ShareFatIcon} from "@phosphor-icons/react";
-import {useEffect, useState} from "react";
-import {useLocation, useNavigate} from "react-router-dom";
-import {LoginButton} from "./style";
+import { HorseIcon } from "@phosphor-icons/react";
+import { useNavigate } from "react-router-dom";
+import { LoginButton } from "./style";
 
 export const Header = () => {
-	const isLogged = false;
-	const navigate = useNavigate();
-	const location = useLocation();
-	const [isOnHomePage, setIsOnHomePage] = useState(false);
+  const isLogged = false;
+  const navigate = useNavigate();
 
-	useEffect(() => {
-		if (location.pathname == "/") setIsOnHomePage(true);
-		else setIsOnHomePage(false);
-	}, [location]);
+  function handleClickButtonLogin() {
+    navigate("/login");
+  }
 
-	function handleClickButtonLogin() {
-		navigate("/login");
-	}
-	function handleClickButtonBack() {
-		navigate(-1);
-	}
-	return (
-		<header className="w-full flex justify-between p-6 bg-transparent">
-			<div className="flex items-center justify-between w-1/4">
-				<button>
-					{!isOnHomePage && (
-						<ShareFatIcon
-							size={52}
-							className="rotate-y-180 cursor-pointer text-[var(--color-primary)] hover:text-[var(--color-accent)] transition-all active:scale-95"
-							weight="fill"
-							onClick={handleClickButtonBack}
-						/>
-					)}
-				</button>
-				<h1 className="font-jersey text-6xl">42ChessClub</h1>
-			</div>
-
-			{isLogged ? (
-				<HorseIcon
-					size={52}
-					className="cursor-pointer hover:scale-105 transition-all"
-				/>
-			) : (
-				<LoginButton onClick={handleClickButtonLogin}>Login</LoginButton>
-			)}
-		</header>
-	);
+  return (
+    <header className="w-full flex flex-col md:flex-row md:justify-between p-6 bg-transparent">
+      <a
+        href="/"
+        className="font-jersey text-center text-6xl duration-500 transition-all hover:translate-x-5"
+      >
+        42ChessClub
+      </a>
+      {isLogged ? (
+        <HorseIcon
+          size={52}
+          className="cursor-pointer hover:scale-105 transition-all"
+        />
+      ) : (
+        <LoginButton onClick={handleClickButtonLogin}>Login</LoginButton>
+      )}
+    </header>
+  );
 };
--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,17 +1,19 @@
-import { Outlet } from 'react-router-dom';
-import { Header } from '../components/Header';
-import Queen from '../components/Queen';
-import { Footer } from '../components/Footer';
+import { Outlet } from "react-router-dom";
+import { Header } from "../components/Header";
+import Queen from "../components/Queen";
+import { Footer } from "../components/Footer";
 
 export const DefaultLayout = () => {
-	return (
-		<div className="bg-[var(--color-background)]">
-			<Header />
-			<main className="relative w-screen">
-				<Queen />
-				<Outlet />
-			</main>
-			<Footer />
-		</div>
-	);
+  return (
+    <div className="bg-[var(--color-background)]">
+      <Header />
+      <main className="relative w-screen">
+        <div className="hidden lg:block">
+          <Queen />
+        </div>
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
 };
