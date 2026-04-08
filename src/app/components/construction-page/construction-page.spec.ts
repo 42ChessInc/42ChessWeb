656ff8c0320d8db2cbc6411bf3711627a9ca3202
denionline <danielximenes164@gmail.com>
2026-04-08 16:10:05 +0100

    feat: Create the construction component for general use.

---
+++ src/app/components/construction-page/construction-page.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+
+import { ConstructionPage } from "./construction-page";
+
+describe("ConstructionPage", () => {
+	let component: ConstructionPage;
+	let fixture: ComponentFixture<ConstructionPage>;
+
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			imports: [ConstructionPage],
+		}).compileComponents();
+
+		fixture = TestBed.createComponent(ConstructionPage);
+		component = fixture.componentInstance;
+		await fixture.whenStable();
+	});
+
+	it("should create", () => {
+		expect(component).toBeTruthy();
+	});
+});
