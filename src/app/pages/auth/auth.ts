2199cb483ea81b038fc4c3d3c49d7c8307779752
denionline <danielximenes164@gmail.com>
2026-04-10 12:48:46 +0100

    feat: Set style and the input components on auth form

--- src/app/pages/auth/auth.ts
+++ src/app/pages/auth/auth.ts
@@ -1,9 +1,12 @@
 import { Component } from "@angular/core";
+import { EmailInput } from "./components/email-input/email-input";
+import { PasswordInput } from "./components/password-input/password-input";
+import { UsernameInput } from "./components/username-input/username-input";
 
 @Component({
 	selector: "app-auth",
-	imports: [],
+	imports: [EmailInput, PasswordInput, UsernameInput],
 	templateUrl: "./auth.html",
 	styleUrl: "./auth.css",
 })
-export class Auth {}
+export class Auth { }
