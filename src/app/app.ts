b0e10f6733ebb337c06842884a6ff0d711d14ff8
denionline <danielximenes164@gmail.com>
2026-04-10 17:25:15 +0100

    feat: Implement the new component toast at root of the app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
+import { NgxSonnerToaster, toast } from "ngx-sonner";
 
 @Component({
 	selector: "app-root",
-	imports: [RouterOutlet],
+	imports: [RouterOutlet, NgxSonnerToaster],
 	templateUrl: "./app.html",
 	styleUrl: "./app.css",
 })
 export class App {
 	protected readonly title = signal("42ChessWeb");
+	protected readonly toast = toast;
 }
