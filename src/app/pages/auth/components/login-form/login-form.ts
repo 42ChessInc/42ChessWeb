7b4eeb6f793d8159ce14d1f4cd9fc6a3f747bbe5
denionline <danielximenes164@gmail.com>
2026-04-10 15:44:14 +0100

    feat: Start to implement the form service

--- src/app/pages/auth/components/login-form/login-form.ts
+++ src/app/pages/auth/components/login-form/login-form.ts
@@ -1,8 +1,25 @@
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Component } from "@angular/core";
+
 
 @Component({
 	selector: "app-login-form",
 	imports: [],
 	templateUrl: "./login-form.html",
 })
-export class LoginForm {}
+export class LoginForm {
+	loginForm: FormGroup;
+
+	constructor(private fb: FormBuilder) {
+		this.loginForm = this.fb.group({
+			email: ['', Validators.required, Validators.email],
+			password: ['', Validators.required, Validators.minLength(8)]
+		})
+	}
+
+	onSubmit() {
+		if (this.loginForm.valid) {
+			console.log(this.loginForm.value)
+		}
+	}
+}
