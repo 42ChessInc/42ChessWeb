42b41f5df574a689f4796de72d99d4d751f88ed4
denionline <danielximenes164@gmail.com>
2026-04-10 12:48:10 +0100

    feat: New username component for the register form

---
+++ src/app/pages/auth/components/username-input/username-input.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+
+import { UsernameInput } from "./username-input";
+
+describe("UsernameInput", () => {
+	let component: UsernameInput;
+	let fixture: ComponentFixture<UsernameInput>;
+
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			imports: [UsernameInput],
+		}).compileComponents();
+
+		fixture = TestBed.createComponent(UsernameInput);
+		component = fixture.componentInstance;
+		await fixture.whenStable();
+	});
+
+	it("should create", () => {
+		expect(component).toBeTruthy();
+	});
+});
---
+++ src/app/pages/auth/components/username-input/username-input.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+	selector: "app-username-input",
+	imports: [],
+	templateUrl: "./username-input.html",
+	styleUrl: "./username-input.css",
+})
+export class UsernameInput {}
