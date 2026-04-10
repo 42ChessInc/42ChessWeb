3725a2d9a0e8688d23f2b901d46386c2f4bfe37d
denionline <danielximenes164@gmail.com>
2026-04-10 14:44:24 +0100

    rm: Removed input components to replace to form components

--- src/app/pages/auth/components/password-input/password-input.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-	selector: "app-password-input",
-	imports: [],
-	templateUrl: "./password-input.html",
-})
-export class PasswordInput {}
--- src/app/pages/auth/components/username-input/username-input.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-	selector: "app-username-input",
-	imports: [],
-	templateUrl: "./username-input.html",
-})
-export class UsernameInput {}
