461a0ca9af6e9db1dfc02d326f8090468264431c
denionline <danielximenes164@gmail.com>
2026-04-13 20:29:34 +0100

    style: Standardize quotes and improve formatting in login form component

--- src/app/pages/auth/components/login-form/login-form.ts
+++ src/app/pages/auth/components/login-form/login-form.ts
@@ -1,8 +1,7 @@
-import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from "@angular/forms";
 import { Component } from "@angular/core";
-import { toast } from 'ngx-sonner';
-import { AuthService } from '../../../../services/auth-service';
-
+import { toast } from "ngx-sonner";
+import { AuthService } from "../../../../services/auth-service";
 
 @Component({
 	selector: "app-login-form",
@@ -12,24 +11,28 @@ import { AuthService } from '../../../../services/auth-service';
 export class LoginForm {
 	loginForm: FormGroup;
 
-	constructor(private fb: FormBuilder, private authService: AuthService) {
+	constructor(
+		private fb: FormBuilder,
+		private authService: AuthService,
+	) {
 		this.loginForm = this.fb.group({
-			email: ['', [Validators.required, Validators.email]],
-			password: ['', [Validators.required, Validators.minLength(8)]]
-		})
+			email: ["", [Validators.required, Validators.email]],
+			password: ["", [Validators.required, Validators.minLength(8)]],
+		});
 	}
 
 	onSubmit() {
-		// if (!this.loginForm.valid) return;
+		if (!this.loginForm.valid) return;
 		this.authService.login(this.loginForm.value).subscribe({
-			next: (res) => {
-				console.log('Success', res);
-				toast.success('sucesso');
+			next: (token) => {
+				localStorage.setItem("token", token);
+				console.log("Success");
+				toast.success("sucesso");
 			},
 			error: (err) => {
-				console.error('Error', err);
-				toast.error('error');
-			}
+				console.error("Error", err);
+				toast.error("Invalid email or password");
+			},
 		});
 	}
 }
